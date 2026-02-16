1b3866cf2458bff52dc826c191a939c998f1b077
quangtm198-pt <EMAIL_GITHUB_MI>
2026-02-16 18:26:56 +0700

    chuyen sang supabase

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -3,8 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SECRET_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
 
 /* ===== GET ===== */
 export async function GET() {

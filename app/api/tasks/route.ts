1d12bfb41290fcd3be8dd7a98c60093d47b08ea3
quangtm198-pt <EMAIL_GITHUB_MI>
2026-02-22 18:46:18 +0700

    update full project

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 export const dynamic = "force-dynamic";
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL);
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,

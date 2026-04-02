f392aed9c7e4accf4bcc039be0367749475ed86d
quangtm198-pt <EMAIL_GITHUB_MI>
2026-04-03 00:07:17 +0700

    update full project

--- app/thong-ke/page.tsx
+++ app/thong-ke/page.tsx
@@ -162,7 +162,9 @@ export default function ThongKePage() {
       case "HTNV":
         return "bg-yellow-100 text-yellow-800";
 
-   
+        default:
+          return "bg-red-100 text-red-700";
+  
 
     }
 

a96bb911deae6a8dfa08fce01f9536ba6138c531
quangtm198-pt <EMAIL_GITHUB_MI>
2026-04-03 08:59:05 +0700

    update full project

--- app/thong-ke/page.tsx
+++ app/thong-ke/page.tsx
@@ -154,16 +154,16 @@ export default function ThongKePage() {
     switch (xepLoai) {
 
       case "HTXSNV":
-        return "bg-green-100 text-green-700";
+        return "bg-greem-100 text-green-700";
 
       case "HTTNV":
-        return "bg-green-100 text-green-700";
+        return "bg-yellow-100 text-orange-700";
 
       case "HTNV":
-        return "bg-yellow-100 text-yellow-800";
+        return "bg-red-100 text-red-800";
 
         default:
-          return "bg-red-100 text-red-700";
+          return "bg-gray-100 text-gray-700";
   
 
     }

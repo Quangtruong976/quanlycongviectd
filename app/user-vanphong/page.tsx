ca56c1874ef72f38f0b3111589f9b935f21bb40b
quangtm198-pt <EMAIL_GITHUB_MI>
2026-04-05 20:59:39 +0700

    update full project

--- app/user-vanphong/page.tsx
+++ app/user-vanphong/page.tsx
@@ -400,29 +400,42 @@ onChange={(e)=>update(i,"san_pham",e.target.value)}
 
 <td className="border p-1">
 <input
-type="date"
-disabled={!t.created_by_user || !t.isEditing}
+type={t.ngay_giao ? "date" : "text"}
+placeholder="Nhập ngày giao việc"
 className={`w-full ${!t.ngay_giao ? "text-red-400" : "text-black"}`}
 value={t.ngay_giao || ""}
+onFocus={(e)=> e.target.type="date"}
+onBlur={(e)=>{
+  if(!t.ngay_giao) e.target.type="text";
+}}
 onChange={(e)=>update(i,"ngay_giao",e.target.value)}
 />
 </td>
 
 <td className="border p-1">
 <input
-type="date"
-disabled={!t.created_by_user || !t.isEditing}
+type={t.han_hoan_thanh ? "date" : "text"}
+placeholder="Nhập hạn hoàn thành"
 className={`w-full ${!t.han_hoan_thanh ? "text-red-400" : "text-black"}`}
 value={t.han_hoan_thanh || ""}
+onFocus={(e)=> e.target.type="date"}
+onBlur={(e)=>{
+  if(!t.han_hoan_thanh) e.target.type="text";
+}}
 onChange={(e)=>update(i,"han_hoan_thanh",e.target.value)}
 />
 </td>
 
 <td className="border p-1">
 <input
-type="date"
+type={t.ngay_hoan_thanh ? "date" : "text"}
+placeholder="Nhập ngày hoàn thành"
 className={`w-full ${!t.ngay_hoan_thanh ? "text-red-400" : "text-black"}`}
 value={t.ngay_hoan_thanh || ""}
+onFocus={(e)=> e.target.type="date"}
+onBlur={(e)=>{
+  if(!t.ngay_hoan_thanh) e.target.type="text";
+}}
 onChange={(e)=>update(i,"ngay_hoan_thanh",e.target.value)}
 />
 </td>
